eb2def2a46effaa76db23b2033f1f177dc74fdba
mikimaine <mikias.amdu@gmail.com>
2020-08-30 22:23:21 +0300

    bcrypt

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,12 +1,12 @@
 import { Bristol } from 'bristol'
 import palin from 'palin'
-import { env } from './env'
+// import { env } from './env'
 
 export const logger = new Bristol()
 
 /* istanbul ignore next */
-if (env.LOG_LEVEL !== 'off') {
-  logger.addTarget('console').withFormatter(palin, {
-    rootFolderName: 'koa-starter' // Edit this to match your actual foldername
-  })
-}
+// if (env.LOG_LEVEL !== 'off') {
+logger.addTarget('console').withFormatter(palin, {
+  rootFolderName: 'koa-starter' // Edit this to match your actual foldername
+})
+// }
