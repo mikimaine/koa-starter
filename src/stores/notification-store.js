e8d16909e36bd012ffe7e92d7c18b11121689d60
mikimaine <mikias.amdu@gmail.com>
2020-09-06 13:44:18 +0300

    Refactor notification store

--- src/stores/notification-store.js
+++ src/stores/notification-store.js
@@ -13,6 +13,11 @@ export default function createTripStore(logger, userModel, notificationModel) {
   const population = [{ path: 'user', select: userModel.attributes }]
 
   return {
+    /**
+     *
+     *
+     * @returns
+     */
     async find() {
       logger.debug(`Finding ${collectionName}`)
       return model.find({}, null, {
@@ -20,7 +25,13 @@ export default function createTripStore(logger, userModel, notificationModel) {
         populate: population
       })
     },
-
+    /**
+     *
+     *
+     * @param {*} query
+     * @param {*} options
+     * @returns
+     */
     async paginate(query, options) {
       logger.debug(`Finding and paginating ${collectionName}`)
       return model.paginate(
@@ -28,7 +39,12 @@ export default function createTripStore(logger, userModel, notificationModel) {
         Object.assign(options, { select: returnFields, populate: population })
       )
     },
-
+    /**
+     *
+     *
+     * @param {*} id
+     * @returns
+     */
     async get(id) {
       logger.debug(`Getting ${collectionName} with id ${id}`)
       const found = await model
@@ -45,62 +61,86 @@ export default function createTripStore(logger, userModel, notificationModel) {
       }
       return found
     },
-
+    /**
+     *
+     *
+     * @param {*} ids
+     * @returns
+     */
+    async findMany(ids) {
+      logger.debug(`Getting ${collectionName} with ids`)
+      const founds = await model.find(
+        {
+          _id: {
+            $in: ids
+          }
+        },
+        null,
+        {
+          select: returnFields,
+          populate: population
+        }
+      )
+      if (!founds) {
+        return null
+      }
+      return founds
+    },
+    /**
+     *
+     *
+     * @param {*} data
+     * @returns
+     */
     async getBy(data) {
       logger.debug(`Getting ${collectionName}`)
-      const found = await model.findOne(data, returnFields)
+      const found = await model
+        .findOne(data, returnFields)
+        .populate(population)
+        .exec()
       if (!found) {
         return null
       }
       return found
     },
-
-    async findManyBySource(sourceids) {
-      logger.debug(`Getting ${collectionName} with ids`)
-      const founds = await model.find({
-        source_airport_id: {
-          $in: sourceids
-        }
-      })
-      if (!founds) {
-        return null
-      }
-      return founds
-    },
-    async findManyByDestination(sourceids) {
-      logger.debug(`Getting ${collectionName} with ids`)
-      const founds = await model.find({
-        destination_airport_id: {
-          $in: sourceids
-        }
-      })
-      if (!founds) {
-        return null
-      }
-      return founds
-    },
-
+    /**
+     *
+     *
+     * @param {*} data
+     * @returns
+     */
     async create(data) {
       const result = await model.create(data)
       logger.debug(`Created new ${collectionName}`, result)
       return result
     },
-
+    /**
+     *
+     *
+     * @param {*} id
+     * @param {*} data
+     * @returns
+     */
     async update(id, data) {
       const result = await model.findOneAndUpdate(
         { _id: id.toString() },
         data,
-        { new: true, select: returnFields }
+        { new: true, select: returnFields, populate: population }
       )
 
       logger.debug(`Updated ${collectionName} ${id}`, result)
       return result
     },
-
-    async remove(model) {
-      model.remove()
-      logger.debug(`Removed ${collectionName} ${model._id}`)
-      return model
+    /**
+     *
+     *
+     * @param {*} item
+     * @returns
+     */
+    async remove(item) {
+      item.remove()
+      logger.debug(`Removed ${collectionName} ${item._id}`)
+      return item
     }
   }
 }
