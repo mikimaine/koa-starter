10af0cf3c9e4fed895035616883f4b14404c970e
mikimaine <mikias.amdu@gmail.com>
2020-09-06 12:43:11 +0300

    create new database connection function

---
+++ src/lib/database.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import { env } from './env'
+import { logger } from './logger'
+
+mongoose.Promise = require('bluebird')
+
+const MONGO_URL = env.MONGO_URL || 'mongodb://localhost/starter'
+
+export const connection = async () => {
+  logger.debug('Connecting to database...')
+  try {
+    const connection = await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    logger.debug('mongo connection created')
+    return connection
+  } catch (err) {
+    logger.debug('Error connecting to Mongo')
+    logger.debug(err)
+  }
+}
