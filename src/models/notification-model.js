456e35482c3a0e9dcb002c3c6d28b1d529089ab3
mikimaine <mikias.amdu@gmail.com>
2020-09-06 13:23:22 +0300

    Convert notification model to container

--- src/models/notification-model.js
+++ src/models/notification-model.js
@@ -1,34 +1,36 @@
 import mongoose from 'mongoose'
 import mongoosePaginate from 'mongoose-paginate'
 
-var Schema = mongoose.Schema
+export default function createRoleModel(logger) {
+  var Schema = mongoose.Schema
 
-/**
- * NotificationSchema
- */
-const NotificationSchema = new Schema({
-  body: { type: String },
-  message: { type: String },
-  user: { type: Schema.Types.ObjectId, ref: 'Users' },
-  status: { type: String, default: 'active' },
-  archived: { type: Boolean, default: false },
-  date_created: { type: Date, default: new Date() },
-  last_modified: { type: Date, default: new Date() }
-})
+  /**
+   * NotificationSchema
+   */
+  const NotificationSchema = new Schema({
+    body: { type: String },
+    message: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: 'Users' },
+    status: { type: String, default: 'active' },
+    archived: { type: Boolean, default: false },
+    date_created: { type: Date, default: new Date() },
+    last_modified: { type: Date, default: new Date() }
+  })
 
-/**
- * Model attributes to expose
- */
-NotificationSchema.statics.attributes = {
-  body: 1,
-  message: 1,
-  user: 1,
-  status: 1,
-  archived: 1,
-  date_created: 1,
-  last_modified: 1
+  /**
+   * Model attributes to expose
+   */
+  NotificationSchema.statics.attributes = {
+    body: 1,
+    message: 1,
+    user: 1,
+    status: 1,
+    archived: 1,
+    date_created: 1,
+    last_modified: 1
+  }
+
+  NotificationSchema.plugin(mongoosePaginate)
+
+  return mongoose.model('Notifications', NotificationSchema)
 }
-
-NotificationSchema.plugin(mongoosePaginate)
-
-export default mongoose.model('Notifications', NotificationSchema)
